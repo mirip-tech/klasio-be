356e7d41f9904540b791783317cb835e98a55955
Hilmi AM <imiia75775@proton.me>
2025-10-19 10:43:30 +0700

    feat: auth register

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:users,email',
+            'password' => ['required', 'string', Rules\Password::defaults()],
+            'device_name' => ['required', 'string'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json($token);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedTokenController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ClassroomController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/auth/register', [RegisteredUserController::class, 'store']);   // register user
 Route::post('/auth/token', [AuthenticatedTokenController::class, 'store']);   // generate token (login)
 Route::delete('/auth/token', [AuthenticatedTokenController::class, 'destroy'])->middleware('auth:sanctum'); // revoke token (logout)
 
