1818a4187d93af04899074721bd75f2b20a89545
Hilmi AM <imiia75775@proton.me>
2025-10-19 10:17:50 +0700

    feat: auth using sanctum only login and logout. register is wip

---
+++ app/Http/Controllers/Auth/AuthenticatedTokenController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthenticatedTokenController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request)
+    {
+       $user = $request->authenticate();
+       $token = $user->createToken($request->device_name)->plainTextToken;
+       return response()->json($token);
+    }
+
+    /**
+     * Destroy current an authenticated token.
+     */
+    public function destroy(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedTokenController;
 use App\Http\Controllers\ClassroomController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/auth/token', [AuthenticatedTokenController::class, 'store']);   // generate token (login)
+Route::delete('/auth/token', [AuthenticatedTokenController::class, 'destroy'])->middleware('auth:sanctum'); // revoke token (logout)
 
 Route::apiResource('classroom', ClassroomController::class);
 Route::post('classroom/{classroom}/enroll', [ClassroomController::class, 'enroll']);
